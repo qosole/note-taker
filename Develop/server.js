422c3c8353a268b04fe721f0561c03e0617fa251
qosole <williamj99wei@gmail.com>
2022-07-26 00:32:13 -0500

    Change main page route to direct from all paths

--- Develop/server.js
+++ Develop/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -10,12 +10,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('public'));
-
-
-// Homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
 
 
 // Main page 
@@ -56,7 +50,14 @@ app.get('/api/notes', (req, res) => {
 
         res.json(JSON.parse(data));
     })
-})
+});
+
+
+
+// Homepage
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 
 app.listen(PORT, () =>
